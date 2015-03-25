9bdd69f707ce9033c6183cc3a388c061baecb930
SatiricRug <pjschinske@gmail.com>
2015-03-25 14:37:17 -0700

    update

--- CSIA/src/main/java/satiric_rug/csia/init/ModBlocks.java
+++ CSIA/src/main/java/satiric_rug/csia/init/ModBlocks.java
@@ -2,12 +2,13 @@ package satiric_rug.csia.init;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import satiric_rug.csia.block.BlockPortal;
+import satiric_rug.csia.reference.Names;
 
 public class ModBlocks {
 
 	public static final BlockPortal portal = new BlockPortal();
 	
 	public static void init() {
-		GameRegistry.registerBlock(portal, "portal");
+		GameRegistry.registerBlock(portal, Names.Blocks.PORTAL);
 	}
 }
